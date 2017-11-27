50e8f32efd25d4d1d6eeaf4c2aae90acc3121c9e
Erik Doernenburg <erik@doernenburg.com>
2017-11-27 23:52:36 +0100

    Caching pairings list.

--- src/main/java/com/mullekybernetik/gagame/tournament/AllPairingsFactory.java
+++ src/main/java/com/mullekybernetik/gagame/tournament/AllPairingsFactory.java
@@ -7,9 +7,17 @@ import java.util.List;
 
 public class AllPairingsFactory implements PairingFactory {
 
+    List<Pairing> pairings;
+
     @Override
     public List<Pairing> getPairingsForStrategies(List<Strategy> strategies) {
-        List<Pairing> pairings = new ArrayList<>();
+
+        int s = strategies.size();
+        if ((pairings != null) && (pairings.size() == ((s*s)-s)/2)) {
+            return pairings;
+        }
+
+        pairings = new ArrayList<>();
         for (int i = 0; i < strategies.size(); i++) {
             for (int j = i + 1; j < strategies.size(); j++) {
                 pairings.add(new Pairing(i, j));
